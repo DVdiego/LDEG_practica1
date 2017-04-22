8fb84e7f4c1bcfb96d13f3f02d7fc589c0441ba7
Diego Jimeno <diegojimeno1997@gmail.com>
2017-04-22 19:35:12 +0200

    Revert "solución de un pequeño fallo"
    This reverts commit a9634679a9278a2d6bdfb88b7e12e0895352bc4b.

--- TAD_LDE/src/Lineales/LDEG.java
+++ TAD_LDE/src/Lineales/LDEG.java
@@ -48,7 +48,7 @@ public class LDEG<E> implements ListaDE<E> {
     }
 
     @Override
-    public void insertarEnFin(E x) {
+    public void insertarEnFin(Object x) {
         NodoLDE<E> nuevo = new NodoLDE<E>(x);
         NodoLDE<E> aux = primero;
         while(aux.siguiente!=null){
